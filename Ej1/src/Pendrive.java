380ce66b725969c68aa9d080bd0441f5a889d723
Sebakira13 <sebastian.calderonl@mail.udp.cl>
2016-03-11 11:08:30 -0300

    Added files via upload

---
+++ Ej1/src/Pendrive.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author cetecom
+ *
+ */
+import java.util.ArrayList;
+
+
+public class Pendrive {
+
+	private static double[][] arregloB = {{}};
+	
+	
+	public Pendrive (double arregloB[][])
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
