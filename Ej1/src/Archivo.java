47599605c48cc768620ccb67f882dde9703cd36a
Sebakira13 <sebastian.calderonl@mail.udp.cl>
2016-03-14 10:27:10 -0300

    Create Archivo.java

---
+++ Ej1/src/Archivo.java
@@ -0,0 +1,25 @@
+public class Archivo
+{
+  private String Nombre;
+  private float PesoMB;
+  
+    public Archivo(String n, float pmb)
+    {
+      Nombre=n;
+      PesoMB=pmb;
+    }
+    
+  public String getNombre()
+  {
+    return Nombre;
+  }
+  
+  public float getPesoMB()
+  {
+    return Peso;
+  }
+  
+  
+
+
+}
