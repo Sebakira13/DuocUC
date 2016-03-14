765f53ccc1b0707fa448e36c66eae7e6eb0d6bbc
Sebakira13 <sebastian.calderonl@mail.udp.cl>
2016-03-14 10:10:04 -0300

    Update Pendrive.java

--- Ej1/src/Pendrive.java
+++ Ej1/src/Pendrive.java
@@ -1,31 +1,60 @@
-/**
- * 
- */
-
-/**
- * @author cetecom
- *
- */
-import java.util.ArrayList;
-
 
 public class Pendrive {
-
-	private static double[][] arregloB = {{}};
 	
+	private String marca;
+	private double totalOcupado;
+	private int cantidadArchivos;
 	
-	public Pendrive (double arregloB[][])
-	{
+	public Pendrive (String m, double to, int ca){
 		
-		
+		marca=m;
+		totalOcupado=to;
+		cantidadArchivos=ca;
 		
 	}
 	
+	public void setDisminuir(double valor)
+	{
+		totalOcupado= totalOcupado-valor;
+	}
 	
+	public void setArchivoMenos()
+	{
+		cantidadArchivos=cantidadArchivos-1;
+	}
+	
+	public void setVirus()
+	{
+		totalOcupado=totalOcupado*0.1;
+	}
+	
+	public String getMarca()
+	{
+		return marca;
+	}
+	
+	public double getTotalOcupado()
+	{
+		return totalOcupado;
+	}
+	
+	public int getTotalArchivos()
+	{
+		return cantidadArchivos;
+	}
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Pendrive p1 = new Pendrive("kingston",2024,10);
+		p1.setDisminuir(1000);
+		System.out.println(p1.getTotalOcupado());
+		p1.setArchivoMenos();
+		System.out.println(p1.getTotalArchivos());
+		System.out.println(p1.getMarca());
+		p1.setVirus();
+		System.out.println(p1.getTotalOcupado());
+		
 
 	}
 
